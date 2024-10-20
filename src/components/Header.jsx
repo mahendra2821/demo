e4351469cd42b2c494db0215a5b4853949b50858
jammulamahendra28210702@gmail.com <jammulamahendra28210702@gmail.com>
2024-10-20 23:55:14 +0530

    button changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
     <Slider/>
     <div className='Navbar flex font-serif'>
         <div className="">
-        <h1 className="mt-4 ml-[5vw] mr-[15vw] hidden lg:block md:block font-bold text-black">
+        <h1 className="mt-4 ml-[5vw] mr-[15vw] hidden lg:block md:block font-bold text-black text-[30px]">
   Nostra
 </h1>     
    </div>
